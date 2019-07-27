7c8f8ab970887bffd21d5732be2f1b4c97443620
Max <mks59k@gmail.com>
2019-07-27 23:43:37 +0300

    Добавлено 3 компонента, настроен роутинг

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProductComponent } from './components/product/product.component';
+import {RouterModule} from '@angular/router';
+
+const routes = [
+  { path: '', component: ProfileComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'product', component: ProductComponent},
+  { path: '**', component: ProductComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
